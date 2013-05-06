72284e269686989aad4b84d2dd50104f6194f170
Dmitry Alexeenko <dalexeenko@gmail.com>
2013-05-05 23:06:40 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,32 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
   end
 
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.json
   def new
   	@user = User.new(params[:user])
     if @user.save
@@ -12,14 +36,68 @@ class UsersController < ApplicationController
     end
   end
 
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
   def create
     @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
+
+    respond_to do |format|
+      if @user.save
+        format.html {
+          sign_in @user
+          redirect_to @user, notice: 'Welcome to the Sample App!'
+        }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html {
+          flash[:success] = "Profile updated"
+          sign_in @user
+          redirect_to @user
+        }
+        format.json { head :no_content }
+      else
+        format.html { render 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
