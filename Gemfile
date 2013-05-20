b99daf63229f8f81171ed68746909b52a4dce7ca
Dmitry Alexeenko <dalexeenko@gmail.com>
2013-05-19 17:04:53 -0700

    Scheduler changes

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
 gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.6'
+gem 'whenever', :require => false
+gem "feedzirra"
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- app/models/article.rb
+++ app/models/article.rb
@@ -18,4 +18,19 @@
 class Article < ActiveRecord::Base
   attr_accessible :article_url, :author, :body, :image_url, :preview, :preview_chunks, :source_id, :title
   has_many :chunks
+
+  def self.update_from_feed(feed_url)
+  	feed_url = "http://feeds.feedburner.com/TechCrunch/"
+  	feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+  	feed.entries.each do |entry|
+  		unless exists? :article_url => entry.url
+  			create!(
+  				:title => entry.title,
+  				:preview => entry.summary,
+  				:article_url => entry.url,
+  				:body => entry.content
+  			)
+  		end
+  	end
+  end
 end
