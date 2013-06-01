f6c926f142b1ac0670efd6f37056326a4f2f8af2
Dmitry Alexeenko <dalexeenko@gmail.com>
2013-06-01 13:02:03 -0700

    Slice articles into chunks

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,21 +24,61 @@ class Article < ActiveRecord::Base
   attr_accessible :article_url, :author, :body, :image_url, :preview, :preview_chunks, :source_id, :title
   has_many :chunks
 
+  MAX_CHUNK_SIZE = 300
+
+  def self.split_into_sub_sentences string
+    sub_sentences = split_into_tokens string, '[\.\,:;]'
+    
+    sub_sentences.inject([]){|b,j|
+      if !b.last || (b.last.length + j.length > MAX_CHUNK_SIZE) then
+        b << j
+      else
+        b.last << " #{j}"
+      end
+      b
+    }
+  end
+
+  def self.split_into_chunks body
+    sentences = split_into_tokens body, '[\.\?!]'
+
+    sentences.inject([]){|a,i|
+      if !a.last || (a.last.length + i.length > MAX_CHUNK_SIZE) then
+        if i.length < MAX_CHUNK_SIZE
+          a << i
+        else
+          sub_sentences = split_into_sub_sentences i
+
+          sub_sentences.each do |part|
+            a << part
+          end
+        end
+      else
+        a.last << " #{i}"
+      end
+      a
+    }
+  end
+
+  def self.split_into_tokens(string, separator)
+    string.scan(/(.+?#{separator}) ?/).map(&:first)
+  end
+
   def self.update_from_feed(feed_url)
-  	feed = Feedzirra::Feed.fetch_and_parse(feed_url)
-  	feed.entries.each do |entry|
-  		unless exists? :article_url => entry.entry_id
-  			create!(
-  				:source_id => 13,
-  				:author => entry.author,
-  				:title => entry.title,
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+    feed.entries.each do |entry|
+      unless exists? :article_url => entry.entry_id
+        create!(
+          :source_id => 13,
+          :author => entry.author,
+          :title => entry.title,
           :image_url => (Nokogiri(entry.summary)/"img").map{ |i| i['src'] },
-  				:preview => ActionView::Base.full_sanitizer.sanitize(entry.summary),
-  				:article_url => entry.entry_id,
-  				:body => ActionView::Base.full_sanitizer.sanitize(entry.content)
-  			)
-  		end
-  	end
+          :preview => ActionView::Base.full_sanitizer.sanitize(entry.summary),
+          :article_url => entry.entry_id,
+          :body => ActionView::Base.full_sanitizer.sanitize(entry.content)
+        )
+      end
+    end
   end
 
   def self.populate_articles
@@ -52,7 +92,7 @@ class Article < ActiveRecord::Base
 
       Chunk.create!(:article_id => article.id, :audio_url => title_url, :body => title)
 
-      preview = article.preview.scan(/.{1,300}/m)
+      preview = split_into_chunks article.preview
 
       number_of_preview_chunks = 0
 
@@ -62,7 +102,7 @@ class Article < ActiveRecord::Base
         number_of_preview_chunks += 1
       end
 
-      body = article.body.scan(/.{1,300}/m)
+      body = split_into_chunks article.body
 
       body.each do |body_chunk|
         url = self.generate_audio(body_chunk)
