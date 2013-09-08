1701dbcf5a69408158be1514cb42bf17133023c2
Dmitry Alexeenko <dalexeenko@gmail.com>
2013-09-08 16:04:07 -0700

    Changing mp3 conversion settings + code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,108 @@
-# Ruby on Rails Tutorial: sample application
+# ListenApp server code
 
-This is the sample application for
-[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
-by [Michael Hartl](http://michaelhartl.com/).
+This is the server part of ListenApp app. Its purpose is to regularly fetch
+articles from specified RSS feeds, generate audio chunks for them and save
+articles and chunks in the database.
+
+Most requests to ListenApp REST API (in particular requests to ListenApp
+resources) require client to authenticate. So let’s say we have Crystal who
+downloaded the app and ran it for the first time - in this case the app makes an
+anonymous request to the server API:
+
+GET /articles.json HTTP/1.1
+Host: talkieapp-staging.herokuapp.com
+Accept: application/json
+
+I will also include cURL commands to show how to make these requests from the
+terminal:
+
+$ curl -v -H "Accept: application/json" https://talkieapp-staging.herokuapp.com/articles.json
+
+The server will force user to auth by rejecting the request with a 401
+(Unauthorized) status code as shown below:
+
+HTTP/1.1 401 Unauthorized
+
+In this case the client will need to present user a sign-up page to allow her to
+authenticate. In the first version of the app we will make this transparent to
+the user - so, instead of showing the sign-up page, the app will automatically
+issue a request to sign up (passing a JSON structure that contains user
+information):
+
+POST /users.json HTTP/1.1
+Host: talkieapp-staging.herokuapp.com
+Accept: application/json
+Content-type: application/json
+
+{
+  "user": {
+    "email": "nicki@gmail.com",
+    “name”: “nicki”,
+    "password": "starships",
+    "password_confirmation": "starships"
+  }
+}
+
+$ curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"user":{"email":"nicki@gmail.com", "name":"nicki", "password":"starships", "password_confirmation":"starships"}}' https://talkieapp-staging.herokuapp.com/users.json
+
+The server then will try to add this user to the database validating two
+constraints: uniqueness of user’s email & uniqueness of user’s device id. If
+either of these already exists in the database, the server will issue a 422
+(Unprocessable entity) like so:
+
+HTTP/1.1 422 Unprocessable entity
+
+If the validation passes, then a new user object will be created and the server
+will respond with 201 (Created):
+
+HTTP/1.1 201 Created 
+Location: https://talkieapp-staging.herokuapp.com/users/1
+
+{
+  "admin":null,
+  "created_at":"2013-05-19T19:12:16Z",
+  "email":"nicki@gmail.com",
+  "id":1,"name":"nicki",
+  "password_digest":"$2a$10$lyo8pBhRMUL.Tc/kbi5TCuU06m3..ZrWBtZ.BesomLYXPG7W6y",
+  "remember_token":"ruAMVQcutaHb19SdJFWnYA",
+  "updated_at":"2013-05-19T19:12:16Z"
+}
+
+This user information will be then used to create a new session (i.e., sign in):
+
+POST /sessions.json HTTP/1.1
+Host: talkieapp-staging.herokuapp.com
+Accept: application/json
+Content-type: application/json
+
+{
+  “session”:
+    {
+      “email”: “nicki@gmail.com”,
+      “password”: “starships”
+    }
+}
+
+$ curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -c cookies -d '{"session":{"email":"nicki@gmail.com", "password":"starships"}}' https://talkieapp-staging.herokuapp.com/sessions.json
+
+If authorization succeeds, the response will have the cookie with user’s token
+that the client will need to store:
+
+HTTP/1.1 302 Found 
+Content-Type: application/json; charset=utf-8
+Set-Cookie: remember_token=zYgDdhQd0EXqGZgcf9e1RQ; path=/; expires=Thu, 19-May-2033 19:26:19 GMT; secure
+
+After that the client can make requests to access Talkie resources:
+
+GET /articles.json HTTP/1.1
+Host: talkieapp-staging.herokuapp.com
+Cookie: remember_token=zYgDdhQd0EXqGZgcf9e1RQ
+Accept: application/json
+
+$ curl -v -H "Accept: application/json" -b cookies https://talkieapp-staging.herokuapp.com/articles.json
+
+This will return the list of articles.
+
+If non admin user tries to add an article, they will get 403 (Forbidden):
+
+$ curl -v -H "Accept: application/json" -H "Content-type: application/json" -b cookies -X POST -d '{"article":{"source_id":13, "author":"Nicki Minaj", "title":"Starships", "preview":"Let\u0027s go to the beach, each. Let\u0027s go get away.", "image_url":"image_url", "article_url":"article_url", "body":"They say, what they gonna say? Have a drink, clink, found the bud light. Bad bitches like me, is hard to come by. The patron on, let\u0027s go get it on. The zone on, yes, I\u0027m in the zone. Is it two, three? Leave a good tip. I\u0027mma blow off my money and don\u0027t give two shits", "preview_chunks":3}}' https://talkieapp-staging.herokuapp.com/articles.json
--- app/models/article.rb
+++ app/models/article.rb
@@ -15,25 +15,38 @@
 #  updated_at     :datetime         not null
 #
 
+require 'addressable/uri'
+require 'htmlentities'
 require "net/http"
 require "net/https"
-require "uri"
 require 'open-uri'
 require 's3'
-require 'htmlentities'
 require 'stanford-core-nlp'
-require 'addressable/uri'
+require "uri"
 
 include ActionView::Helpers::TextHelper
 
 class Article < ActiveRecord::Base
-  attr_accessible :article_url, :author, :body, :image_url, :preview, :preview_chunks, :source_id, :title, :published_at
+  attr_accessible :article_url
+  attr_accessible :author
+  attr_accessible :body
+  attr_accessible :image_url
+  attr_accessible :preview
+  attr_accessible :preview_chunks
+  attr_accessible :published_at
+  attr_accessible :source_id
+  attr_accessible :title
+
   has_many :chunks
 
+  # Max number of characters in a single chunk
   MAX_CHUNK_SIZE = 300
 
+  # Max number of characters in an article summary
   SUMMARY_SIZE = 800
 
+  # Splits given piece of text into array with sentences
+  #
   def self.split_into_sentences text
     StanfordCoreNLP.jar_path = '/app/bin/stanford-core-nlp/'
     StanfordCoreNLP.model_path = '/app/bin/stanford-core-nlp/'
@@ -44,11 +57,18 @@ class Article < ActiveRecord::Base
     text.get(:sentences).to_a.map &:to_s
   end
 
+  # Splits given piece of text into chunks
+  #
   def self.split_into_chunks text
     sentences = split_into_sentences text
-    sentences = sentences.map {|sentence| sentence.length > MAX_CHUNK_SIZE ? word_wrap(sentence, :line_width => MAX_CHUNK_SIZE).split("\n") : sentence }.flatten
 
-    sentences.reduce([]){|a,i|
+    sentences = sentences.map { |sentence|
+      sentence.length > MAX_CHUNK_SIZE ?
+        word_wrap(sentence, :line_width => MAX_CHUNK_SIZE).split("\n") :
+        sentence
+    }.flatten
+
+    sentences.reduce([]){ |a, i|
       if !a.last || (a.last.length + i.length > MAX_CHUNK_SIZE) then
           a << i
       else
@@ -58,8 +78,10 @@ class Article < ActiveRecord::Base
     }
   end
 
-  def self.generate_summary content
-    sentences = split_into_sentences content
+  # Extracts summary from an article
+  #
+  def self.extract_summary text
+    sentences = split_into_sentences text
 
     sentences.reduce([]){|a,i|
       if !a.last || (a.last.length + i.length > SUMMARY_SIZE) then
@@ -71,6 +93,8 @@ class Article < ActiveRecord::Base
     }
   end
 
+  # Fetches articles from RSS feed and saves them into the database
+  #
   def self.update_from_feed(feed_url)
     feed = Feedzirra::Feed.fetch_and_parse(feed_url)
 
@@ -81,13 +105,16 @@ class Article < ActiveRecord::Base
 
         source = Source.all :conditions => { :rss_url => feed_url }
 
-        if (!(Nokogiri(entry.summary)/"img").at_css("img").nil?) && (source.first.name.include? "TechCrunch") then
-            image_url = Addressable::URI.parse((Nokogiri(entry.summary)/"img").at_css("img")['src'])
-            params = image_url.query_values
-            params.delete('crop')
-            params['w'] = (params['w'].to_i * 2).to_s
-            params['h'] = (params['h'].to_i * 2).to_s
-            image_url.query_values = params
+        if (!(Nokogiri(entry.summary)/"img").at_css("img").nil?) &&
+          (source.first.name.include? "TechCrunch") then
+          image_url = Addressable::URI.parse(
+            (Nokogiri(entry.summary)/"img").at_css("img")['src'])
+
+          params = image_url.query_values
+          params.delete('crop')
+          params['w'] = (params['w'].to_i * 2).to_s
+          params['h'] = (params['h'].to_i * 2).to_s
+          image_url.query_values = params
         else
           if source.first.name.include? "TechCrunch" then
             image_url = "https://talkieapp.s3.amazonaws.com/techcrunch-logo.jpg"
@@ -117,15 +144,18 @@ class Article < ActiveRecord::Base
         end
 
         if entry.content.nil? then
-          c = entry.summary
+          content = entry.summary
         else
-          c = entry.content
+          content = entry.content
         end
 
-        content = HTMLEntities.new.decode(ActionView::Base.full_sanitizer.strip_tags(c).squish)
-        sentences = generate_summary content
+        content = HTMLEntities.new.decode(
+          ActionView::Base.full_sanitizer.strip_tags(content).squish)
+
+        sentences = extract_summary content
         summary = sentences[0]
 
+        # Skip the article if there is no article content (body)
         next if content.nil? || content.length == 0
 
         content.slice! summary
@@ -134,9 +164,6 @@ class Article < ActiveRecord::Base
         if source.first.name.include? "Washington Post" then
           summary.slice! "Read full article \u003E\u003E"
         end
-
-        puts "Title: " + entry.title.strip + "\n"
-        puts "Summary: " + summary + "\n"
 
         create!(
           :source_id => source.first.id,
@@ -152,94 +179,56 @@ class Article < ActiveRecord::Base
     end
   end
 
+  # Fetches articles from RSS feeds and generates audio chunks for them
+  #
   def self.populate_articles
-    articles = Article.find :all,
-                            :order => 'id desc',
-                            :conditions => "preview_chunks IS NULL"
+    articles = Article.find
+      :all,
+      :order => 'id desc',
+      :conditions => "preview_chunks IS NULL"
 
     articles.each do |article|
       begin
-
-        r = rand(5)
-
-        if article.source_id == 13 then
-          if r == 0 then
-            voice = 'Allison'
-          elsif r == 1 then
-            voice = 'Carol'
-          elsif r == 2 then
-            voice = 'Samantha'
-          elsif r == 3 then
-            voice = 'Tom'
-          elsif r == 4 then
-            voice = 'Carol'
-          end
-        else          
-          if r == 0 then
-            voice = 'crystal'
-          elsif r == 1 then
-            voice = 'mike'
-          elsif r == 2 then
-            voice = 'rich'
-          elsif r == 3 then
-            voice = 'lauren'
-          elsif r == 4 then
-            voice = 'claire'
-          end
-        end
-
         title = article.title
-
-        if article.source_id == 13 then
-          title_url = self.generate_audio_bing(title, voice)
-        elsif article.source_id == 19 then
-          title_url = self.generate_audio_bing(title, voice)
-        else
-          title_url = self.generate_audio_bing(title, voice)
-        end
-
-        Chunk.create!(:article_id => article.id, :audio_url => title_url, :body => title)
+        title_url = self.generate_audio_bing(title, "voice")
+        Chunk.create!(
+          :article_id => article.id,
+          :audio_url => title_url,
+          :body => title)
 
         preview = split_into_chunks article.preview
-
         number_of_preview_chunks = 0
 
         preview.each do |preview_chunk|
-          if article.source_id == 13 then
-            url = self.generate_audio_bing(preview_chunk, voice)
-          elsif article.source_id == 19 then
-            url = self.generate_audio_bing(preview_chunk, voice)
-          else
-            url = self.generate_audio_bing(preview_chunk, voice)
-          end
-
-          Chunk.create!(:article_id => article.id, :audio_url => url, :body => preview_chunk)
+          url = self.generate_audio_bing(preview_chunk, "voice")
+          Chunk.create!(
+            :article_id => article.id,
+            :audio_url => url,
+            :body => preview_chunk)
           number_of_preview_chunks += 1
         end
 
         body = split_into_chunks article.body
 
         body.each do |body_chunk|
-          if article.source_id == 13 then
-            url = self.generate_audio_bing(body_chunk, voice)
-          elsif article.source_id == 19 then
-            url = self.generate_audio_bing(body_chunk, voice)
-          else
-            url = self.generate_audio_bing(body_chunk, voice)
-          end
-          Chunk.create!(:article_id => article.id, :audio_url => url, :body => body_chunk)
+          url = self.generate_audio_bing(body_chunk, "voice")
+          Chunk.create!(
+            :article_id => article.id,
+            :audio_url => url,
+            :body => body_chunk)
         end
 
         article.preview_chunks = number_of_preview_chunks
         article.save
       rescue ArgumentError
-        puts "Exception!"
+        puts "Something went wrong!"
         article.preview_chunks = -1
         article.save
       end
     end
   end
 
+  # AT&T text-to-speech
   def self.generate_audio(text, voice)
     uri = URI.parse("http://192.20.225.36/tts/cgi-bin/nph-nvdemo")
 
@@ -249,16 +238,12 @@ class Article < ActiveRecord::Base
     response = http.request(request)
     redirectLocation = response['location']
 
-    # $ curl -v -H "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q-H "Content-Type:application/x-www-form-urlencoded"
-    # -X POST -d 'voice=crystal&txt=Access+Notifications+On+The+Lock+Screen+A+minor+but+ongoing+frustration+with+iOS’s+earlier+
-    # implementation+of+the+Notification+Center+is+that+it+required+you+to+unlock+your+device.&speakButton=SPEAK' http://192.20.225.36/tts/cgi-bin/nph-nvdemo
-    #This interactive text-to-speech site is provided by AT&T solely
-    # for demonstration purposes.  Any distribution, professional, or commercial use is
-    # strictly disallowed.
+    raise ArgumentError, "redirect location is nil! request: " + request.to_s +
+      "; text: " + text if redirectLocation.nil?
 
-    raise ArgumentError, "redirect location is nil! request: " + request.to_s + "; text: " + text if redirectLocation.nil?
-
-    amazon = S3::Service.new(access_key_id: 'AKIAJMGKXIP5RHBHSMMA', secret_access_key: '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')
+    amazon = S3::Service.new(
+      access_key_id: 'AKIAJMGKXIP5RHBHSMMA',
+      secret_access_key: '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')
     bucket = amazon.buckets.find('talkieapp')
     url = 'http://192.20.225.36' + redirectLocation
 
@@ -283,6 +268,7 @@ class Article < ActiveRecord::Base
     file.url
   end
 
+  # Loquendo/Nuance text-to-speech
   def self.generate_audio_nuance(text, voice)
     puts "Voice used = " + voice
     uri = URI.parse("https://tts.nuancemobility.net:443/NMDPTTSCmdServlet/tts")
@@ -292,15 +278,25 @@ class Article < ActiveRecord::Base
 
     request = Net::HTTP::Get.new(uri.request_uri)
 
-    request.set_form_data({"codec" => "wav", voice => voice, "ttsLang" => "en_US", "text" => text, "appId" => "NMDPTRIAL_dmitry_alexeenko20130627022038", "appKey" => "79711b1aad792b84364c85d3f633551286d6645570b657959c272dd0c6449e3fb5b959623384e769d06c3cc69f91d925673d7bd7f3d26d8ffa9a60c0defe0093", "id" => "0000"})
+    request.set_form_data(
+      {
+        "codec" => "wav",
+        voice => voice,
+        "ttsLang" => "en_US",
+        "text" => text,
+        "appId" => "NMDPTRIAL_dmitry_alexeenko20130627022038",
+        "appKey" => "79711b1aad792b84364c85d3f633551286d6645570b657959c272dd0c6449e3fb5b959623384e769d06c3cc69f91d925673d7bd7f3d26d8ffa9a60c0defe0093",
+        "id" => "0000"})
 
     request = Net::HTTP::Get.new(uri.request_uri + '?' + request.body)
 
     response = http.request(request)
 
-    raise ArgumentError, "Exceeded maximum number of transactions per day." if response.code == "503"
+    raise ArgumentError, "Exceeded max number of transactions per day." if response.code == "503"
 
-    amazon = S3::Service.new(access_key_id: 'AKIAJMGKXIP5RHBHSMMA', secret_access_key: '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')
+    amazon = S3::Service.new(
+      access_key_id: 'AKIAJMGKXIP5RHBHSMMA', 
+      secret_access_key: '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')
     bucket = amazon.buckets.find('talkieapp')
 
     open("tempfile_nuance.wav", "wb") do |file|
@@ -326,6 +322,7 @@ class Article < ActiveRecord::Base
     file.url
   end
 
+  # Bing text-to-speech
   def self.generate_audio_bing(text, voice)
     clientID = "listenapp"
     clientSecret = "Q7fSU9UNs1lNbEtxzMcozmAp/hB6vocQoGoxwpOlGJg="
@@ -339,7 +336,14 @@ class Article < ActiveRecord::Base
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Post.new(uri.request_uri)
-    request.set_form_data({"client_id" => clientID, "client_secret" => clientSecret, "grant_type" => grantType, "scope" => scopeUrl})
+    request.set_form_data(
+      {
+        "client_id" => clientID,
+        "client_secret" => clientSecret,
+        "grant_type" => grantType,
+        "scope" => scopeUrl
+      })
+
     response = http.request(request)
 
     access_token = JSON.parse(response.body)["access_token"]
@@ -349,18 +353,39 @@ class Article < ActiveRecord::Base
 
     request = Net::HTTP::Get.new(uri.request_uri)
 
-    request.set_form_data({"text" => text, "language" => "en", "format" => "audio/mp3"})
+    request.set_form_data(
+      {
+        "text" => text,
+        "language" => "en",
+        "format" => "audio/mp3",
+        "options" => "minsize"
+      })
 
     request = Net::HTTP::Get.new(uri.request_uri + '?' + request.body)
     request["Authorization"] = "Bearer " + access_token
 
     response = http.request(request)
 
-    amazon = S3::Service.new(access_key_id: 'AKIAJMGKXIP5RHBHSMMA', secret_access_key: '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')
+    amazon = S3::Service.new(
+      access_key_id: 'AKIAJMGKXIP5RHBHSMMA',
+      secret_access_key: '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')
     bucket = amazon.buckets.find('talkieapp')
 
+    open("tempfile_bing.mp3", "wb") do |file|
+        file.write(response.body)
+    end    
+
+    wavfile = Tempfile.new(".mp3")
+    wavfile.binmode
+
+    open("tempfile_bing.mp3", "rb") do |file|
+      wavfile << file.read
+    end
+
+    mp3 = convert_tempfile(wavfile)
+
     file = bucket.objects.build(SecureRandom.uuid + '.mp3')
-    file.content = response.body
+    file.content = (File.read mp3)
 
     if file.save
       print file.url
@@ -372,7 +397,20 @@ class Article < ActiveRecord::Base
   def self.convert_tempfile(tempfile)
     dst = Tempfile.new(".mp3")
 
-    cmd_args = [File.expand_path(tempfile.path), File.expand_path(dst.path)]
+    # Converting mp3 to mp3 with smallest file size and reasonable quality
+    # See http://macintoshhowto.com/audio/how-to-compress-speech.html for
+    # more info regarding lame parameters
+    cmd_args = [
+      "--mp3input",
+      "--abr",
+      "16",
+      "-q",
+      "0",
+      "-m",
+      "m",
+      File.expand_path(tempfile.path),
+      File.expand_path(dst.path)]
+
     system("bin/lame", *cmd_args)
 
     dst.binmode
